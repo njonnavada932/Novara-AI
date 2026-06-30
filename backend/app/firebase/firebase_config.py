456c4aea57c1dd61776cc9e5d9c0b94a5ab82e3d
njonnavada <neelimajonnavada@gmail.com>
2026-06-30 21:04:39 +0530

    Use Secret Manager for Firebase

--- backend/app/firebase/firebase_config.py
+++ backend/app/firebase/firebase_config.py
@@ -1,13 +1,17 @@
+from pathlib import Path
 import firebase_admin
-from firebase_admin import credentials, auth
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
-# Prevent multiple initialization
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    firebase_key_path = "/secrets/firebase-key"
+
+    # Local development
+    if not Path(firebase_key_path).exists():
+        firebase_key_path = "firebase_key.json"
+
+    cred = credentials.Certificate(firebase_key_path)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 print("✅ Firebase initialized")
-print("✅ Firestore client:", db)
